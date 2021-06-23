d12df7e7650e6553260344694f5352e5a8a5a6b0
jjmin321 <jjmin321@naver.com>
2021-06-23 15:27:28 +0900

    [ADD] Function CreateRequest for new request

---
+++ lib/req/createRequest.go
@@ -0,0 +1,27 @@
+package req
+
+import (
+	"SKT_PhoneNumber/config"
+	"SKT_PhoneNumber/lib/log"
+	"net/http"
+
+	"github.com/skratchdot/open-golang/open"
+)
+
+func CreateRequest(url string) *http.Request{
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Err.Fatalln("Request 생성 중 오류가 발생함.")
+	}
+	query := req.URL.Query()
+	query.Add("viewId", config.Config.Skt.ViewId)
+	query.Add("serviceId", config.Config.Skt.ServiceId)
+	query.Add("chg_cd", config.Config.Skt.Chg_cd)
+	query.Add("co_cd", config.Config.Skt.Co_cd)
+	query.Add("line_num_from", config.Config.Skt.Line_num_from)
+	query.Add("line_num_to", config.Config.Skt.Line_num_to)
+	req.URL.RawQuery = query.Encode()
+	open.Start(req.URL.String()) // 브라우저에서 요청하면 됨
+	log.Info.Println(req.URL) // 프로그램에서 요청하면 안 됨
+	return req
+}
